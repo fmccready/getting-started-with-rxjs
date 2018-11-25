3e5bcb4eaf1584262cd5a845d548333cda6002ff
Frank McCready <fmccready@gmail.com>
2018-11-24 23:45:29 -0600

    Add .gitignore

--- index.ts
+++ index.ts
@@ -326,7 +326,11 @@ source$.subscribe(subject$);
 
 console.log('Start script.');
 
-from([1, 2, 3, 4]);
+from([1, 2, 3, 4], queueScheduler).pipe(
+  tap(value => console.log(value)),
+  observeOn(asyncScheduler),
+  tap(value => console.log(`Doubled value: ${value * 2}`))
+).subscribe();
 
 console.log('End script.');
 
